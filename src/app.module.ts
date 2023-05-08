35bb8df98d57825a4f8c226374980a9e3458ebc5
ivanbonnot <ivanbonnot@gmail.com>
2023-05-07 21:48:06 -0300

    cargando DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.xvejx.gcp.mongodb.net/test'),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
